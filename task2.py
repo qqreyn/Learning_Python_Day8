1b9f0fb89b2ad44e4c9a14ca80357fa1ce54bf2b
qqreyn <duch.jan.8@gmail.com>
2026-02-23 16:04:01 +0100

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a + b
+
+num1 = int(input("Enter first number to sum: "))
+num2 = int(input("Enter second number to sum: "))
+
+print("Sum of", num1, "and", num2, "is", add(num1, num2))
